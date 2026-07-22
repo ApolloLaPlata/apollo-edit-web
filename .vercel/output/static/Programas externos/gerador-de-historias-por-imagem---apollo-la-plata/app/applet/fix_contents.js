68abb17901543fd7864d988a87e28d47aa7602d6
Apollo Edit <apollo@laplata.com>
2026-07-21 22:44:37 -0500

    fix: remove .vercel from git to ensure vercel builds from root

--- .vercel/output/static/Programas externos/gerador-de-historias-por-imagem---apollo-la-plata/app/applet/fix_contents.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const file = 'services/geminiService.ts';
-let code = fs.readFileSync(file, 'utf8');
-code = code.replace(/contents: \{ parts \}/g, 'contents: [{ role: "user", parts }]');
-fs.writeFileSync(file, code);
-console.log("Replaced all instances");
